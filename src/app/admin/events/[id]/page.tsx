a73a5f52d5aaa31315c929078593f9a0a9ae0969
Kamal Feracho <kferacho3@users.noreply.github.com>
2025-05-04 16:55:38 -0400

    events again 2

--- src/app/admin/events/[id]/page.tsx
+++ src/app/admin/events/[id]/page.tsx
@@ -1,7 +1,6 @@
 import EventDetails from '@/components/admin/EventDetails';
 import { prisma } from '@/lib/prisma';
 import { notFound } from 'next/navigation';
-import { ReactElement } from 'react';
 
 export const dynamic = 'force-dynamic';
 
@@ -9,14 +8,12 @@ export default async function Page({
   params,
 }: {
   params: { id: string };
-}): Promise<ReactElement> {
+}) {
   const booking = await prisma.booking.findUnique({
-    where: { id: Number(params.id) }, // ← or use params.id directly if your ID column is a string
+    where: { id: Number(params.id) }, // or just `params.id` if your Prisma ID is a string
   });
 
-  if (!booking) {
-    notFound();
-  }
+  if (!booking) notFound();
 
   return <EventDetails booking={booking} />;
 }
